af4c8717c4b98d694559964cf45b17d8bd985f68
Andrei Dobrin <andrei.dobrin@snx.ro>
2022-01-17 13:40:58 +0200

    updated proxy.conf.js

--- ClientApp/proxy.conf.js
+++ ClientApp/proxy.conf.js
@@ -1,13 +1,17 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:44803';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:44803';
+
+const target = "https://localhost:44467/";
 
 const PROXY_CONFIG = [
   {
     context: [
+      "/images",
       "/weatherforecast",
       "/error",
+      "/favicon.ico",
       "/_configuration",
       "/.well-known",
       "/Identity",
